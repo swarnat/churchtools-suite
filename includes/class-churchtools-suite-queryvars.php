1a875fa03e915b3276b6c47cd463816288536e0c
Stefan Warnat <ich@stefanwarnat.de>
2026-02-22 12:32:15 +0100

    docs: cleanup page docblock for query vars

--- includes/class-churchtools-suite-queryvars.php
+++ includes/class-churchtools-suite-queryvars.php
@@ -3,11 +3,8 @@
  * Query variable Class
  * 
  * Simple wrapper around the wordpress query variables
- * Only logs errors and warnings in production, all levels in WP_DEBUG mode.
  *
  * @package ChurchTools_Suite
- * @since   0.3.13.3
- * @version 1.1.4.2 Simplified to use WordPress error_log()
  */
 
 if (!defined('ABSPATH')) {
